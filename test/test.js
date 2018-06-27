edf0e6f73cac3de10bf2ae2d37e3d6935aa6c9dc
Tomas Korenko <radixxko@gmail.com>
2018-06-27 13:26:47 +0200

    Version 1.0.0

--- test/test.js
+++ test/test.js
@@ -4,25 +4,54 @@ const Style = require('../lib/style');
 
 const style =
 `
+/* janko hrasko */
 $font: 'Tahoma', 'Helvetica' ;
+$font3: $font2;
+$font2: $font;
+$color: red;
 @border-radius{ border-radius: 5px; }
+@box{ top:0; left:0; right:0; bottom:0; color: $color; }
 html, body
 {
 	margin: 0;
 	$color: rgba(255,0,0,0.2);
 
+	&:hover, &:focus{ $color:magenta; color: $color; @box; }
+
 	$font: 'Tahoma', 'Helvetica' ;
-	a
+	a, b
 	{
-		@border-radius;
-		padding: 0;
+		padding: 6px;
+		padding: 7px;
+		@border-radius; // jozo je tu
+		padding: 0px;
+		padding: 5px;
+		@box;
+
+		&:hover
+		{
+			color: red;
+
+			.active
+			{
+				color: blue;
+			}
+		}
+
+		body.noscroll &{ color:silver; }
 	}
 }
 `;
 
-let start = process.hrtime();
-let compiled = Style.compile( style );
+Style.init();
+
+let start = process.hrtime(), i, compiled;
+
+for( i = 0; i < 1000; ++i )
+{
+	compiled = Style.compile( style );
+}
 let end = process.hrtime(start);
 
-console.log( JSON.stringify( compiled, null, '  ' ) );
-console.log( (end[0] + end[1]/1e9).toFixed(3) + 's' );
+console.log( compiled );
+console.log( ( (end[0] + end[1]/1e9) * 1000 / i ).toFixed(2) + 'ms' );
